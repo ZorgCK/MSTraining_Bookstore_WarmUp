3eda0a7ac821565c5c9563517ed0fc6c0206ea9f
Kümmel Christian <c.kuemmel@microstream.one>
2023-05-15 12:40:26 +0200

    zc

--- src/main/java/one/microstream/controller/BookController.java
+++ src/main/java/one/microstream/controller/BookController.java
@@ -2,6 +2,7 @@ package one.microstream.controller;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.micronaut.http.HttpResponse;
@@ -9,18 +10,31 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import one.microstream.domain.Author;
 import one.microstream.domain.Book;
+import one.microstream.reference.Lazy;
+import one.microstream.storage.DB;
 import one.microstream.utils.MockupUtils;
 
 
 @Controller("/books")
 public class BookController
 {
-	@Get("/create")
-	public HttpResponse<?> createBooks()
+	@Get("/setupData")
+	public HttpResponse<?> setupData()
 	{
 		List<Book> allCreatedBooks = MockupUtils.loadMockupData();
 		
-		// Enter your code here
+		allCreatedBooks.forEach(b ->
+		{
+			DB.root.getBooks().computeIfAbsent(
+				b.getIsbn().substring(0, 2),
+				k -> Lazy.Reference(new ArrayList<>())).get().add(b);
+			
+			DB.root.getAuthors().computeIfAbsent(
+				b.getAuthor().getId() + ":" + b.getAuthor().getEmail() + ":" + b.getAuthor().getLastname(),
+				k -> Lazy.Reference(b.getAuthor()));
+		});
+		
+		DB.storageManager.storeAll(DB.root.getBooks(), DB.root.getAuthors());
 		
 		return HttpResponse.ok("Books successfully created!");
 	}
@@ -31,13 +45,21 @@ public class BookController
 		Author author = new Author("100", "John", "Doe", "j.doe@example.com", "Male");
 		Book book = new Book("123456789", "Single Book", LocalDate.now(), new BigDecimal(13.32), author);
 		
-		// Enter your code here
-				
+		DB.root.getBooks().computeIfAbsent(
+			book.getIsbn().substring(0, 2),
+			k -> Lazy.Reference(new ArrayList<>())).get().add(book);
+		
+		DB.root.getAuthors().computeIfAbsent(
+			book.getAuthor().getId() + ":" + book.getAuthor().getEmail() + ":" + book.getAuthor().getLastname(),
+			k -> Lazy.Reference(book.getAuthor()));
+		
+		DB.storageManager.storeAll(DB.root.getBooks(), DB.root.getAuthors());
+		
 		return HttpResponse.ok("Book successfully created!");
 	}
 	
 	@Get
-	public List<Book> getBook()
+	public List<Book> getBooks()
 	{
 		// Enter your code here
 	}
--- src/main/java/one/microstream/storage/DB.java
+++ src/main/java/one/microstream/storage/DB.java
@@ -1,6 +1,31 @@
 package one.microstream.storage;
 
+import java.time.Duration;
+
+import one.microstream.domain.Root;
+import one.microstream.reference.Lazy;
+import one.microstream.reference.LazyReferenceManager;
+import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+
+
 public class DB
 {
+	public static EmbeddedStorageManager	storageManager;
+	public final static Root				root	= new Root();
 	
+	static
+	{
+		LazyReferenceManager.set(LazyReferenceManager.New(
+			Lazy.Checker(Duration.ofMinutes(10).toMillis())
+			)).start();
+		
+		storageManager = EmbeddedStorageConfiguration.Builder()
+			.setChannelCount(2)
+			.setStorageDirectory("data")
+			.setBackupDirectory("backup")
+			.createEmbeddedStorageFoundation()
+			.createEmbeddedStorageManager(root)
+			.start();
+	}
 }
